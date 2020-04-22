6f55fa9f93d3df70965804d8d7302b843e6a2439
FajarBN <fajarbarrun@gmail.com>
2020-04-22 17:12:10 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sapiente obcaecati deserunt totam at neque qui ullam voluptate blanditiis quia itaque voluptatibus illo similique recusandae quae, provident et, quas doloribus!</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
